c7b146a56554b80d5f80f9f69489f000b5fc6689
Didi518 <kevin.ouali@gmail.com>
2022-01-14 17:49:18 +0100

    csv

--- import.php
+++ import.php
@@ -1,4 +1,9 @@
 <?php
+//gestion des erreurs
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // connecter DB
 $dbhost = "localhost";
 $dbname = "simplon_test";
